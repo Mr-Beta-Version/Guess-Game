bae61ad5a5870bad4723bc1878921266812a4de8
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2026-04-24 12:56:55 +0600

    Add files via upload

---
+++ guess-script.js
@@ -0,0 +1,16 @@
+let key = 1456;
+
+let userkey = prompt("Guess the number : ");
+
+while(key!=userkey){
+    if (userkey>key){
+        userkey = prompt("You Enter A Big Number. Try Again");
+    }else if (userkey<key){
+        userkey = prompt("You Enter A Small Number. Try Again");
+    }
+    else{
+        userkey = prompt("Guess a number bullshit");
+    }
+}
+
+console.log("Congratulations You Win The Game");
